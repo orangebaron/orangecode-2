1eafb4733da86f42e008330163f693c1df588b3f
orangebaron <orangebaronroblox@gmail.com>
2018-06-25 20:42:40 -0400

    reorganized; split into lines function; modified split symbols function

--- code.hs
+++ code.hs
@@ -18,33 +18,52 @@ priorityInlineSymbols = "._"
 areaOpeners = "{[(\"'"
 areaClosers = "}])\"'"
 
-splitLine :: String -> [String]
-splitLine line = foldr (\x t -> splitSymbols x ++ t) [] (separateEnclosedAreas "" line)
+hasOpener :: String -> Bool
+hasOpener "" = False
+hasOpener str = elem (head str) areaOpeners
 
-splitSymbols :: String -> [String] --split away symbols: "a+b" -> ["a","+","b"]; strings starting with areaOpeners are ignored; priorityInlineSymbols are ignored
-splitSymbols line
-  | (line == "") || (elem (head line) areaOpeners) = [line]
-  | otherwise = S.split (S.dropBlanks $ S.oneOf inlineSymbols) line
+doubleHead :: [a] -> [a]
+doubleHead (x:y:_) = [x,y]
+doubleHead [x] = [x]
+doubleHead [] = []
+
+doubleTail :: [a] -> [a]
+doubleTail (_:_:x) = x
+doubleTail _ = []
+
+splitSymbols :: [String] -> [String] --split away symbols: ["a+b"] -> ["a","+","b"]; strings starting with areaOpeners are ignored; priorityInlineSymbols are ignored
+splitSymbols line = foldr (\x t -> f x ++ t) [] line where
+  f :: String -> [String]
+  f part
+    | (part == "") || (hasOpener part) = [part]
+    | otherwise = S.split (S.dropBlanks $ S.oneOf inlineSymbols) part
+
+splitIntoLines :: [String] -> [[String]]
+splitIntoLines strings = foldr (\x t -> f x t) [] strings where
+  addToLines :: [String] -> [[String]] -> [[String]]
+  addToLines [] acc = acc
+  addToLines [""] acc = acc
+  addToLines [str] acc@(h:t)
+    | elem (last str) lineEnders = [str]:acc
+    | otherwise = (str:h):t
+  addToLines str@(h1:t1) acc = addToLines [h1] $ addToLines t1 acc
+  f :: String -> [[String]] -> [[String]]
+  f str [] = foldr (\x t -> [x]:t) []
+    (S.split (S.dropInitBlank $ S.dropFinalBlank $ S.dropInnerBlanks $ S.keepDelimsR $ S.oneOf lineEnders) str)
+  f str acc@(h:t)
+    | (str == "") || (hasOpener str) = (str:h):t
+    | otherwise = addToLines (S.split (S.keepDelimsR $ S.oneOf lineEnders) str) acc
 
 separateEnclosedAreas :: [Char] -> String -> [String] --separate areas enclosed by {} [] () "" or ''
 separateEnclosedAreas closers area --closers are the symbols that are needed to close the areas: '{'->'}' etc; they stack, with newer ones at the start
   | area == "" = []
   | head area == '\\' = qualifiedJoinOnto closers (doubleHead area) (separateEnclosedAreas closers (doubleTail area))
-  | (elem (head area) areaOpeners) && ((closers == "") || head closers /= respectiveCloser) =
+  | (hasOpener area) && ((closers == "") || head closers /= respectiveCloser) =
     joinOnto (head area) (separateEnclosedAreas (respectiveCloser:closers) (tail area))
   | (closers == "") || (head area /= head closers) = qualifiedJoinOnto closers [head area] (separateEnclosedAreas closers (tail area))
   | otherwise = joinAreaEnd closers (head area) (separateEnclosedAreas (tail closers) (tail area))
     where
       respectiveCloser = areaClosers !! unMaybe (L.elemIndex (head area) areaOpeners) 0
-
-      doubleHead :: [a] -> [a]
-      doubleHead (x:y:_) = [x,y]
-      doubleHead [x] = [x]
-      doubleHead [] = []
-
-      doubleTail :: [a] -> [a]
-      doubleTail (_:_:x) = x
-      doubleTail _ = []
 
       joinOnto :: Char -> [String] -> [String]
       joinOnto c (x:y) = (c:x):y
